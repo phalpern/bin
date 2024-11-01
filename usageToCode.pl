7b0b2678a85f55e3941e279da608b5b3de1cce3b
Pablo Halpern <phalpern@halpernwightsoftware.com>
2024-11-01 13:54:26 -0400

    Change `usageToCode.pl` to account for bad indenation in header comment.

--- usageToCode.pl
+++ usageToCode.pl
@@ -22,29 +22,41 @@ if (@ARGV)
 my $line;
 my $isCode = 0;
 my $exampleNum = 0;
+my $extraIndent = undef;
 while (defined($line = <STDIN>))
 {
     $line =~ s/assert/ASSERT/g;
 
-    if ($line =~ m(^// ?```) or $line =~ m(^//\.\.))
-    {
+    if ($line =~ m(^// ?```) or $line =~ m(^//\.\.)) {
         $isCode = ! $isCode;
+        $extraIndent = undef;
         print "// ```\n";
         next;
     }
 
     if ($isCode)
     {
-        $line =~ s(^//)(  );
+        $line =~ s(^// *$)();  # Empty comment line
+        $line =~ s(^//)(  );   # Uncomment start of line
+
+        # If indent length is one less than a multiple of 4 (i.e., the
+        # remainder is 3), then add an extra space of indent at the start of
+        # the line to bring it to normal indentation.  If $extraIndent is set
+        # to true for a code section, then it remains true for the remainder of
+        # the section.
+        if (! defined($extraIndent) and $line =~ m(^( +)[^ ])) {
+            $extraIndent = (length($1) % 4 == 3);
+        }
+
+        $line = ' ' . $line if $extraIndent;
 
         # Comment out '#include' within test driver (it should go at the top)
         $line =~ s(^    #include)(//  #include);
 
-        if ($line =~ m/\bmain\(/ )
-        {
+        if ($line =~ m/\bmain\(/ ) {
             ++$exampleNum;
             $line =~ s/\bint +main\(/void usageExample$exampleNum\(/x ;
         }
     }
-    print $line
+    print $line;
 }
